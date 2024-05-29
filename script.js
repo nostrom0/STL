d0c59fabe9527268ecd7b5d35f155a035c1f72b7
nostrom0 <nostromo_16@hotmail.com>
2024-05-30 00:13:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    loadImages('DragonBall', 'images/dragonball', 100);
-    loadImages('StarWars', 'images/StarWars', 3); // Ajusta el número de imágenes según lo necesario
+    loadImages('dragonball', 'images/dragonball', 100);
+    loadImages('starwars', 'images/StarWars', 3); // Ajusta el número de imágenes según lo necesario
     loadImages('panel3', 'images/panel3', 0); // Ajusta el número de imágenes según lo necesario
 
     // Cerrar la lightbox al hacer clic fuera de la imagen
@@ -32,10 +32,10 @@ function closeLightbox() {
 }
 
 function loadImages(panelId, imagePath, imageCount) {
-    var grid = document.getElementById(panelId.toLowerCase() + '-grid'); // Convertir a minúsculas
+    var grid = document.getElementById(panelId + '-grid'); // Consistentes con los IDs en HTML
 
     for (let i = 1; i <= imageCount; i++) {
-        let imageName = `${panelId}(${i})`;
+        let imageName = `${capitalizeFirstLetter(panelId)}(${i})`;
         let imgPath = `${imagePath}/${imageName}.png`;
 
         let gridItem = document.createElement('div');
@@ -54,4 +54,8 @@ function loadImages(panelId, imagePath, imageCount) {
 
         grid.appendChild(gridItem);
     }
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
